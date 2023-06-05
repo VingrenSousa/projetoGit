f28350e4047f004f3c7e4d3006ce0da331fc26f6
vingren <zingenn@gmail.com>
2023-06-05 11:55:22 -0300

    comit botao

---
+++ script.js
@@ -0,0 +1,6 @@
+function botao(){
+    const botao = document.querySelector('div')
+  if( botao.style.background=="red"){
+    botao.style.background='#fff'
+  }else{  botao.style.background='red'}
+}
